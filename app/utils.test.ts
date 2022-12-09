48d3e1e6d37af0c6447352bc28df036ff67c9652
Austin Atchley <austinatchley@gmail.com>
2022-12-08 20:31:44 -0800

    Fix linting in tests

--- app/utils.test.ts
+++ app/utils.test.ts
@@ -1,6 +1,6 @@
-export {}
+// TODO: Remove this placeholder import after I add more tests. It needs to be here to make the linter happy until then
+import { assert } from 'vitest'
 
-// TODO: Add more tests
 test("placeholder test", () => {
     expect(true).toBe(true);
 });
