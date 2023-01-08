a464c622778c21b24316a8fc628fbf99b77b04fb
Austin Atchley <austinatchley@gmail.com>
2023-01-08 00:00:04 -0800

    Add Crypress e2e tests for board and note workflows

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -1,25 +1,58 @@
 import { faker } from "@faker-js/faker";
 
 describe("smoke tests", () => {
-  const mockXPos: number = 1;
-  const mockYPos: number = 2;
+  const testNote = {
+    title: faker.lorem.words(1),
+    body: faker.lorem.sentences(1),
+    xpos: 0,
+    ypos: 0 
+  };
 
-  beforeEach(() => {});
-  afterEach(() => {});
+  beforeEach(() => { });
+  afterEach(() => { });
 
-  it("should allow you to make a note", () => {
-    const testNote = {
-      title: faker.lorem.words(1),
-      body: faker.lorem.sentences(1),
-      xpos: mockXPos,
-      ypos: mockYPos
-    };
+  it("should allow you to view notes", () => {
     cy.visitAndCheck("/");
 
     cy.findByRole("link", { name: /notes/i }).click();
     cy.findByText("No notes yet");
+  });
 
-    cy.findByRole("link", { name: /\+ new note/i }).click();
+  it("should allow you to view boards", () => {
+    cy.visitAndCheck("/");
+
+    cy.findByRole("link", { name: /boards/i }).click();
+    cy.findByText("No boards yet");
+  });
+
+  it("should allow you to create a board", () => {
+    cy.visitAndCheck("/");
+
+    cy.findByRole("link", { name: /boards/i }).click();
+    cy.findByText("No boards yet");
+
+    cy.findByRole("link", { name: /\+ new board/i }).click();
+
+    cy.findByRole("textbox", { name: /title/i }).type(testNote.title);
+    cy.findByRole("button", { name: /save/i }).click();
+
+    cy.findByRole("button", { name: /delete/i }).click();
+
+    cy.findByText("No boards yet");
+  });
+
+  it("should allow you to create a board and a note", () => {
+    cy.visitAndCheck("/");
+
+    cy.findByRole("link", { name: /boards/i }).click();
+    cy.findByText("No boards yet");
+
+    cy.findByRole("link", { name: /\+ new board/i }).click();
+
+    cy.findByRole("textbox", { name: /title/i }).type(testNote.title);
+    cy.findByRole("button", { name: /save/i }).click();
+
+    cy.contains("Create a new note in this board").click();
 
     cy.findByRole("textbox", { name: /title/i }).type(testNote.title);
     cy.findByRole("textbox", { name: /body/i }).type(testNote.body);
@@ -27,8 +60,15 @@ describe("smoke tests", () => {
     cy.findByRole("textbox", { name: /ypos/i }).type("" + testNote.ypos);
     cy.findByRole("button", { name: /save/i }).click();
 
-    cy.findByRole("button", { name: /delete/i }).click();
+    cy.findAllByText(testNote.title);
 
-    cy.findByText("No notes yet");
+    cy.findByRole("button", { name: /delete/i }).click();
+    cy.findByText("No boards yet");
+
+    cy.visitAndCheck("/");
+    cy.findByRole("link", { name: /notes/i }).click();
+    cy.contains(testNote.title).click();
+
+    cy.findByRole("button", { name: /delete/i }).click();
   });
 });
