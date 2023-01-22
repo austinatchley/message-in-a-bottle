5daea3b3d89d85858a34489a41b8ba8536b29eca
Austin Atchley <austinatchley@gmail.com>
2023-01-22 03:14:02 -0800

    Add new note button to main UI

--- app/routes/boards/$boardId.tsx
+++ app/routes/boards/$boardId.tsx
@@ -67,6 +67,16 @@ export default function BoardDetailsPage() {
       </div>
 
       <hr></hr>
+
+      <div className="p-4">
+          <Link to={`/admin/notes/new?boardId=${data.board.id}`} >
+            <button
+              className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
+            >
+              Create a new note in this board
+            </button>
+          </Link>
+        </div>
     </div>
   );
 }
