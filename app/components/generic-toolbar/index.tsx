141a900621a3bf8ac42bb09b9e37da3a12bc54cb
Austin Atchley <austinatchley@gmail.com>
2023-02-03 23:51:38 -0800

    Put icon into toolbar

--- app/components/generic-toolbar/index.tsx
+++ app/components/generic-toolbar/index.tsx
@@ -9,10 +9,16 @@ interface GenericToolbarProps {
 
 export default function GenericToolbar({ titleMessage, titleLinkUrl, menuMessage, menuLinkUrl }: GenericToolbarProps) {
     return (
-        <div className="flex items-center justify-between bg-slate-800 p-4 text-white">
-            <h1 className="text-left h-8 w-full text-3xl font-bold font-title italic">
-                <Link to={titleLinkUrl}>{titleMessage}</Link>
-            </h1>
+        <div className="flex items-center justify-between bg-slate-800 p-4 text-yellow-400">
+            <Link to={titleLinkUrl} className="flex flex-row w-full">
+                <span className="w-6 h-6 mx-4">
+                    <img src="the_bottle_1.png"></img>
+                </span>
+
+                <h1 className="text-left h-8 w-full text-2xl font-bold font-title italic">
+                    {titleMessage}
+                </h1>
+            </Link>
 
             <div className="flex justify-end items-center bg-slate-500 hover:bg-sky-800 min-w-20">
                 <h1 className="text-2xl px-2 text-stone-100 text-center">
