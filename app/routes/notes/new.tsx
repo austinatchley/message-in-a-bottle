d8bde61a0a78d02783e368690148c4c5b46a9d29
Austin Atchley <austinatchley@gmail.com>
2023-03-10 17:58:40 -0800

    Use consistent styling for note creation

--- app/routes/notes/new.tsx
+++ app/routes/notes/new.tsx
@@ -78,75 +78,77 @@ export default function NewNotePage() {
 
   return (
     <div className="flex flex-col w-full mt-20 max-w-sm mx-auto">
-      <div className="py-4 bg-blue-600 w-full rounded">
-        <label className="text-white pl-4 font-title">
-          <span>
-            New Note
-          </span>
-        </label>
+      <div className="m-auto bg-slate-100 rounded shadow border-slate-800 border-2">
+        <div className="py-4 bg-slate-600 w-full">
+          <label className="text-white pl-4 font-title">
+            <span>
+              New Note
+            </span>
+          </label>
+        </div>
+        <Form
+          method="post"
+          reloadDocument
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 8,
+            width: "100%",
+          }}
+          className="shadow-md rounded px-8 pt-6 pb-8"
+        >
+
+          <div>
+            <label className="flex w-full flex-col gap-1 mb-2">
+              <span className="block text-gray-700 text-md font-bold">Title</span>
+              <input
+                ref={titleRef}
+                name="title"
+                className="flex-1 shadow rounded border border-slate-400 px-3 text-lg leading-loose"
+                aria-invalid={actionData?.errors?.title ? true : undefined}
+                aria-errormessage={
+                  actionData?.errors?.title ? "title-error" : undefined
+                }
+              />
+            </label>
+            {actionData?.errors?.title && (
+              <div className="pt-1 text-red-700" id="title-error">
+                {actionData.errors.title}
+              </div>
+            )}
+          </div>
+
+          <div>
+            <label className="flex w-full flex-col gap-1">
+              <span className="block text-gray-700 text-md font-bold">Message</span>
+              <textarea
+                ref={bodyRef}
+                name="body"
+                rows={8}
+                className="w-full flex-1 shadow rounded border border-slate-400 py-2 px-3 text-lg leading-6"
+                aria-invalid={actionData?.errors?.body ? true : undefined}
+                aria-errormessage={
+                  actionData?.errors?.body ? "body-error" : undefined
+                }
+              />
+            </label>
+            {actionData?.errors?.body && (
+              <div className="pt-1 text-red-700" id="body-error">
+                {actionData.errors.body}
+              </div>
+            )}
+          </div>
+
+          <div className="text-right mt-6">
+            <button
+              type="submit"
+              className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
+            >
+              Save
+            </button>
+          </div>
+        </Form>
       </div>
-      <Form
-        method="post"
-        reloadDocument
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          gap: 8,
-          width: "100%",
-        }}
-        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-      >
-
-        <div>
-          <label className="flex w-full flex-col gap-1 mb-2">
-            <span className="block text-gray-700 text-md font-bold">Title</span>
-            <input
-              ref={titleRef}
-              name="title"
-              className="flex-1 shadow rounded border border-slate-400 px-3 text-lg leading-loose"
-              aria-invalid={actionData?.errors?.title ? true : undefined}
-              aria-errormessage={
-                actionData?.errors?.title ? "title-error" : undefined
-              }
-            />
-          </label>
-          {actionData?.errors?.title && (
-            <div className="pt-1 text-red-700" id="title-error">
-              {actionData.errors.title}
-            </div>
-          )}
-        </div>
-
-        <div>
-          <label className="flex w-full flex-col gap-1">
-            <span className="block text-gray-700 text-md font-bold">Message</span>
-            <textarea
-              ref={bodyRef}
-              name="body"
-              rows={8}
-              className="w-full flex-1 shadow rounded border border-slate-400 py-2 px-3 text-lg leading-6"
-              aria-invalid={actionData?.errors?.body ? true : undefined}
-              aria-errormessage={
-                actionData?.errors?.body ? "body-error" : undefined
-              }
-            />
-          </label>
-          {actionData?.errors?.body && (
-            <div className="pt-1 text-red-700" id="body-error">
-              {actionData.errors.body}
-            </div>
-          )}
-        </div>
-
-        <div className="text-right mt-6">
-          <button
-            type="submit"
-            className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
-          >
-            Save
-          </button>
-        </div>
-      </Form>
     </div>
   );
 }
