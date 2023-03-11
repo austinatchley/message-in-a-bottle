4032a44b49e9c9cdf15c890f663bd9f8cc93ba6a
Austin Atchley <austinatchley@gmail.com>
2023-03-10 16:47:50 -0800

    Move landing page buttons into row format and limit page content width to 3xl

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -10,28 +10,30 @@ export const meta: MetaFunction = () => ({
 export default function Index() {
   return (
     <main className="min-h-screen bg-landing-page-background bg-repeat-x bg-slate-800 flex flex-col justify-evenly md:items-center md:justify-center">
-      <div className="mx-auto mt-10 bg-zinc-800">
-        <img src="/images/splash.png"></img>
-      </div>
-
-      <div className="flex flex-col justify-center w-full h-full sm:mt-20 font-medium">
-        <div className="mx-auto text-slate-800 my-8">
-          <Link
-            to="/about"
-            prefetch="intent"
-            className="p-12 text-center rounded-md border shadow-md shadow-amber-200/20 hover:shadow-amber-400/20 bg-slate-200 py-3 hover:bg-slate-300"
-          >
-            About
-          </Link>
+      <div className="max-w-3xl">
+        <div className="mx-auto bg-zinc-800">
+          <img src="/images/splash.png"></img>
         </div>
-        <div className="mx-auto text-slate-800 my-8">
-          <Link
-            to="/boards"
-            prefetch="intent"
-            className="p-20 text-center rounded-md border shadow-lg shadow-amber-300/40 hover:shadow-amber-500/60 bg-gradient-to-bl from-yellow-300 to-amber-300 py-4"
-          >
-            Enter
-          </Link>
+
+        <div className="flex flex-row justify-evenly w-full h-full sm:mt-20 font-medium my-8">
+          <div className="mx-auto text-slate-800">
+            <Link
+              to="/about"
+              prefetch="intent"
+              className="p-20 py-4 text-center rounded-md border shadow-md shadow-amber-200/20 hover:shadow-amber-400/20 bg-slate-200 hover:bg-slate-300"
+            >
+              About
+            </Link>
+          </div>
+          <div className="mx-auto text-slate-800">
+            <Link
+              to="/boards"
+              prefetch="intent"
+              className="p-20 py-4 text-center rounded-md border shadow-lg shadow-amber-300/40 hover:shadow-amber-500/60 bg-gradient-to-bl from-yellow-300 to-amber-300"
+            >
+              Enter
+            </Link>
+          </div>
         </div>
       </div>
     </main>
