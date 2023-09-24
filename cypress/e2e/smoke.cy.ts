9c9e0eb1620034c729e6c139186e2fff82b39969
Austin Atchley <austinatchley@gmail.com>
2023-09-24 17:35:14 -0400

    Update e2e tests

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -20,55 +20,14 @@ describe("smoke tests", () => {
     cy.findByText(/No note selected/); // substring match
   });
 
-  it("should allow you to view bottles", () => {
-    cy.visitAndCheck("/admin");
+  it("should allow you to create a note", () => {
+    cy.visitAndCheck("/bottle");
 
-    cy.findByRole("link", { name: /bottles/i }).click();
-    cy.findByText(/No bottle selected/);
-  });
-
-  it("should allow you to create a bottle", () => {
-    cy.visitAndCheck("/admin");
-
-    cy.findByRole("link", { name: /bottles/i }).click();
-    cy.findByText(/No bottle selected/);
-
-    cy.findByRole("link", { name: /\+ new bottle/i }).click();
-
-    cy.findByRole("textbox", { name: /title/i }).type(testBottle.title);
-    cy.findByRole("button", { name: /save/i }).click();
-
-    cy.findByRole("button", { name: /delete/i }).click();
-
-    cy.findByText(/No bottle selected/);
-  });
-
-  it("should allow you to create a bottle and a note", () => {
-    cy.visitAndCheck("/admin");
-
-    cy.findByRole("link", { name: /bottles/i }).click();
-    cy.findByText(/No bottle selected/);
-
-    cy.findByRole("link", { name: /\+ new bottle/i }).click();
-
-    cy.findByRole("textbox", { name: /title/i }).type(testBottle.title);
-    cy.findByRole("button", { name: /save/i }).click();
-
-    cy.contains("Create a new note in this bottle").click();
+    cy.findByRole("link", { name: /Put/i }).click();
 
     cy.findByRole("textbox", { name: /title/i }).type(testNote.title);
     cy.findByRole("textbox", { name: /message/i }).type(testNote.body);
     cy.findByRole("button", { name: /save/i }).click();
-
-    cy.findAllByText(testNote.title);
-
-    cy.visitAndCheck("/admin");
-    cy.findByRole("link", { name: /bottles/i }).click();
-
-    cy.findByText(testBottle.title, { exact: false }).click();
-    cy.findByRole("button", { name: /delete/i }).click();
-
-    cy.findByText(/No bottle selected/);
 
     cy.visitAndCheck("/admin");
     cy.findByRole("link", { name: /notes/i }).click();
