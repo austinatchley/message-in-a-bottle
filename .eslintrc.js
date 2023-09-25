9dc6e16387f9ca6cca5fe5a52235fe52dedd8e63
Austin Atchley <austinatchley@gmail.com>
2023-09-24 23:48:49 -0400

    Make the "no-unused-vars" linter rule a warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,9 @@ module.exports = {
     "cypress/globals": true,
   },
   plugins: ["cypress"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "warn"
+  },
   // We're using vitest which has a very similar API to jest
   // (so the linting plugins work nicely), but we have to
   // set the jest version explicitly.
