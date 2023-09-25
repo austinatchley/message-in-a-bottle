c063fdf1064a2aa999126d4ff7d4d8ac37d18139
Austin Atchley <austinatchley@gmail.com>
2023-09-25 16:19:28 -0400

    Commit last refactor change to admin portal toolbar

--- app/components/admin-portal-toolbar/index.tsx
+++ app/components/admin-portal-toolbar/index.tsx
@@ -3,7 +3,6 @@ import { GenericToolbar } from "../generic-toolbar";
 export function AdminPortalToolbar() {
   return (
     <GenericToolbar
-      titleMessage="message~in~a~bottle"
       titleLinkUrl="/"
       menuMessage="Admin"
       menuLinkUrl="/admin"
