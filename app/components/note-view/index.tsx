ecb825e4039585398abfd47560643d400f4209d5
Austin Atchley <austinatchley@gmail.com>
2023-09-25 16:01:05 -0400

    Fix date on note view

--- app/components/note-view/index.tsx
+++ app/components/note-view/index.tsx
@@ -1,4 +1,4 @@
-import { Note } from "@prisma/client";
+import type { Note } from "@prisma/client";
 import { parseISO } from "date-fns";
 
 interface NoteProps {
@@ -16,8 +16,7 @@ function formatDate(date: string): string {
 }
 
 export function NoteView({ note }: NoteProps) {
-  const { id, title, body, createdAt } = note;
-  const createdAtString: string = note.createdAt.toString();
+  const { title, body, createdAt } = note;
 
   return (
     <div className="shadow-black-500/50 relative box-border h-64 w-64 border-4 bg-yellow-100 p-4 shadow-lg">
@@ -25,7 +24,7 @@ export function NoteView({ note }: NoteProps) {
       <p className="py-2 font-thin">{body}</p>
       <br></br>
       <div className="absolute inset-x-0 bottom-4 left-1 grid place-items-center">
-        <p className="text-xs font-extralight">{formatDate(createdAtString)}</p>
+        <p className="text-xs font-extralight">{formatDate(createdAt.toISOString())}</p>
       </div>
     </div>
   );
