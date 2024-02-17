eabd31b95339cb710a5f011fa365ba18879d89cc
Austin Atchley <austinatchley@gmail.com>
2024-02-17 18:43:07 -0500

    Improve bottle/index.tsx spacing

--- app/routes/bottle/index.tsx
+++ app/routes/bottle/index.tsx
@@ -2,7 +2,7 @@ import { Link } from "@remix-run/react";
 
 export default function BottleIndexPage() {
   return (
-    <div className="mx-auto flex w-full flex-row sm:justify-center sm:px-32 md:justify-evenly">
+    <div className="mx-auto flex h-full gap-4 py-16 lg:py-48 w-full flex-row justify-evenly sm:px-32">
       <div className="max-w-[18rem] grow border border-theme-accent-1 bg-theme-accent-1 shadow-md hover:shadow-lg">
         <Link to="/notes/new" className="flex h-full flex-col">
           <p className="grow pt-12 text-center font-accent text-4xl">
